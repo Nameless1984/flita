42d05ebb4b23348c573ee07562f63843f926e57e
Nameless1984 <49617771+Nameless1984@users.noreply.github.com>
2022-03-03 11:36:36 +0300

    Update flita.c

--- flita.c
+++ flita.c
@@ -83,6 +83,30 @@ int main()
     }
     
     printf("\n");
+    
+    int size2;
+    printf("Enter the size of the array №2:");
+    printf("\n");
+    scanf("%d", &size2);
+    int *arr2 = (int*)malloc(sizeof(int) * size2);  // Создание динамического массива из size элементов типа int
+    for(int i=0;i<size2;i++)
+    {
+      printf("Enter an element with a number - ");
+      printf("%d",i+1);
+      printf("\n");
+      scanf("%d",&arr2[i]);
+        
+    }
+    
+    printf("Source array №2:");
+    printf("\t");
+    for(int i=0;i<size2;i++)
+    {
+        printf("%d",arr2[i]);
+        printf("\t");
+    }
+    
+    printf("\n");
     //push_back(arr,size,value);
     //pop_back(arr,size+1);
     /*for(int i=0;i<size;i++)
@@ -91,26 +115,80 @@ int main()
         
     }*/
     //size=size+1;
-    int r;
-    printf("Please, enter the element you want to remove");
-    printf("\n");
-    scanf("%d", &r);
-    rem_elt(arr,&size,r);
-    
-    printf("Array after removing: ");
-    for (int i=0; i<size; i++)
+    while(1)
     {
-        printf("%d ",arr[i]);
+        int v;
+        printf("select array(1 or 2)");
+        printf("\n");
+        printf("program exit - 3");
+        printf("\n");
+        scanf("%d",&v);
+        if(v==1)
+        {
+            int d;
+            printf("choose what you want to do: delete or add, 1 - delete, 2- add");
+            printf("\n");
+            scanf("%d",&d);
+            if(d==1)
+            {
+                int r;
+                printf("Please, enter the element you want to remove");
+                printf("\n");
+                scanf("%d", &r);
+                rem_elt(arr,&size,r);
+                
+                printf("Array after removing: ");
+                for (int i=0; i<size; i++)
+                {
+                    printf("%d ",arr[i]);
+                }
+                printf("\n");
+            }
+            if(d==2)
+            {
+                int value;
+                printf("write the number you want to add to the array");
+                printf("\n");
+                scanf("%d", &value);
+                push_back(arr,size,value);
+            }
+        }
+        if(v==2)
+        {
+            int d;
+            printf("choose what you want to do: delete or add, 1 - delete, 2- add");
+            scanf("%d",&d);
+            if(d==1)
+            {
+                int r;
+                printf("Please, enter the element you want to remove");
+                printf("\n");
+                scanf("%d", &r);
+                rem_elt(arr2,&size2,r);
+                
+                printf("Array after removing: ");
+                for (int i=0; i<size2; i++)
+                {
+                    printf("%d ",arr2[i]);
+                }
+                printf("\n");
+            }
+            if(d==2)
+            {
+                int value;
+                printf("write the number you want to add to the array");
+                printf("\n");
+                scanf("%d", &value);
+                push_back(arr2,size2,value);
+            }
+        }
+        if(v==3)
+        {
+            break;
+        }
     }
-    printf("\n");
-    
-    int value;
-    printf("write the number you want to add to the array");
-    printf("\n");
-    scanf("%d", &value);
-    push_back(arr,size,value);
-    
-    free(arr); // Освобождение памяти после использования массива
-
+    free(arr);
+    free(arr2);
     return 0;
 }
+
