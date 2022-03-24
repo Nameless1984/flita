72230cc7c5dcc86a6d9f374ff6e54359809aeb80
Nameless1984 <49617771+Nameless1984@users.noreply.github.com>
2022-03-24 04:52:18 +0300

    Create flita2.c

---
+++ flita2.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+
+    FILE* txt;
+    txt = fopen("adjacency_matrix_90.txt", "r");
+    int V[5][5];
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            V[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        fscanf(txt, "%d %d %d %d %d", &V[i][0], &V[i][1], &V[i][2], &V[i][3], &V[i][4]);
+    }
+    fclose(txt);
+
+    FILE* dot;
+    dot = fopen("g.dot", "a");
+    fprintf(dot, " digraph G {\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j < 5; j++)
+        {
+            for (int m = V[i][j]; m >= 1; m--)
+            {
+                fprintf(dot, "\t%d -> %d\n", i + 1, j + 1);
+            }
+        }
+    }
+    fprintf(dot, "}");
+    fclose(dot);
+    system("dot g.dot -Tpng -og.png");
+    system("pause");
+    return 0;
+}
